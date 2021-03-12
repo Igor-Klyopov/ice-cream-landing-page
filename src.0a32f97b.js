6eb0c70ff685fe46b7cccb21bd2331d5f812756e
alexlex7 <75750709+alexlex7@users.noreply.github.com>
2021-03-12 22:02:10 +0100

    Updates

--- src.0a32f97b.js
+++ src.0a32f97b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-markup-hw-08/src.0a32f97b.js.map
+//# sourceMappingURL=/ice-cream-landing-page/src.0a32f97b.js.map
